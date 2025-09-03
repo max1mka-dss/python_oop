5004bc9ec50d986db10f719e00c98f7adc4c6fe4
max1mka-dss <stebaslo04@gmail.com>
2025-09-03 22:03:14 +0300

    Ignore .idea directory

---
+++ 1.4.6.py
@@ -0,0 +1,18 @@
+class MediaPlayer:
+    color = 'red'
+    circle =2
+    def open(self,file):
+        #self.file = file
+        setattr(self,'filename',file)
+    def play(self,):
+        print(f"Воспроизведение {self.filename}")
+
+media1 = MediaPlayer()
+media1.open('filemedia1')
+#filemedia = 'filemedia'
+
+media2 = MediaPlayer()
+media2.open('filemedia2')
+
+media1.play()
+media2.play()
